990ffec439428cf753d57ee3993d90969740fe5f
student <student@corp.kpiot.ru>
2025-10-15 15:36:22 +0500

    dsadasd

--- tasks/task4.py
+++ tasks/task4.py
@@ -2,9 +2,9 @@
 
 def solve():
 # Ниже пишите решение задачи
-    
-    
-
+  a, b, z = map(int, input().split())
+  result = (a + b > z) and (a + z > b) and (b + z > a) 
+  print(result)
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
     solve()
